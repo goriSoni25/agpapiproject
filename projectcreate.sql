3354ef5ec15319ddb9be00bf8d50022e92677cf5
goriSoni25 <72340910+goriSoni25@users.noreply.github.com>
2021-11-11 13:56:09 +0530

    sql file

---
+++ projectcreate.sql
@@ -0,0 +1,24 @@
+﻿insert into [dbo].[Customer] values 
+(1,'gunnu',12345,'gorisoni09@gmailcom');
+
+
+insert into [dbo].[Tablenumber] values 
+(11,'detail table','comentstable');
+
+insert into [dbo].[staff] values 
+(111,'Abdullah',01);
+
+insert into [dbo].[booking] values 
+(1,13,14,'2038-01-19 03:14:08');
+
+insert into [dbo].[orderr] values 
+(11,123,1234,'pritam','2038-01-19 03:14:08')
+
+insert into [dbo].[staffrole] values 
+(111,'serving','pritam1')
+
+insert into [dbo].[ordermenuitem] values 
+(123,421,'50','foodserving')
+
+insert into [dbo].[ingredient] values 
+('speicy','dry')
---
+++ projectwebapi.sql
@@ -0,0 +1,62 @@
+﻿--create table customer 
+--(
+--customer_id int primary key,
+--customer_name varchar(50) not null,
+--customer_phone varchar(10) not null unique,
+--customer_email varchar(20) not null unique
+--);
+
+--create table Tablenumber(
+--Table_id int primary key,
+--Table_details varchar(50) not null,
+--Table_comments varchar(50) not null
+--);
+--create table staff(
+--staff_id int primary key,
+--staff_name varchar(50) not null,
+--staff_code int not null,
+--);
+
+--create table booking(
+--customer_id int not null,
+--booking_id int primary key not null,
+--table_id varchar(50) not null,
+--date_booking DATETIME not null,
+--foreign key (customer_id) references customer(customer_id)
+--);
+
+--create table orderr(
+--Table_id int not null,
+--order_id int primary key not null,
+--staff_id varchar(50) not null,
+--customer_name varchar(50) not null,
+--order_time DATETIME not null,
+--foreign key (Table_id) references Tablenumber(Table_id)
+--);
+
+--create table staffrole(
+--staff_id int not null,
+--staff_role varchar(50) not null,
+--foreign key (staff_id) references staff(staff_id),
+--staff_name varchar(50) not null
+--);
+--create table ordermenuitem(
+--order_id int not null,
+--menu_item_id int primary key not null,
+--foreign key (order_id) references orderr(order_id),
+--item_quantity varchar(50) not null,
+--order_commment varchar(50) not null
+--);
+
+--create table menu(
+--menu_id int not null,
+--menu_item_id int not null,
+--foreign key (menu_item_id) references ordermenuitem(menu_item_id),
+--item_price varchar not null,
+--other_items varchar not null);
+
+--create table ingredient(
+--ingredient_name varchar(50) not null,
+--ingredient_type varchar(50) not null
+--);
+
